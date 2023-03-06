7ae3e08279b48d28d55ccbfc9a24e1ab21462ab3
Yuzheng Dun <yuzhengdun@Yuzhengs-MacBook-Pro.local>
2023-03-06 00:05:53 -0500

    implement newton's method

--- R/logit_log_likelihood.R
+++ R/logit_log_likelihood.R
@@ -3,14 +3,19 @@ sigmoid = function(x){
 }
 
 logit_log_likelihood = function(beta, design, outcome){
-  prob_1 = sigmoid(design %*% beta)
-  log_likelihood = sum(outcome * log(prob_1) + (1 - outcome) * log(1 - prob_1))
+  pi = sigmoid(design %*% beta)
+  log_likelihood = sum(outcome * log(pi) + (1 - outcome) * log(1 - pi))
   return(log_likelihood)
 }
 
 logit_log_likelihood_gradient = function(beta, design, outcome){
-  prob_1 = sigmoid(design %*% beta)
-  gradient = t(design) %*% (outcome - prob_1)
+  pi = sigmoid(design %*% beta)
+  gradient = t(design) %*% (outcome - pi)
   return(gradient)
 }
 
+logit_log_likelihood_hessian = function(beta, design, outcome){
+  pi = sigmoid(design %*% beta)
+  hessian = -t(design) %*% diag(pi[,1] * (1 - pi[,1])) %*% design
+  return(hessian)
+}
---
+++ R/logit_mle_newton.R
@@ -0,0 +1,25 @@
+are_all_close <- function(v, w, abs_tol = 1e-6, rel_tol = 1e-6) {
+  abs_diff <- abs(v - w)
+  are_all_within_atol <- all(abs_diff < abs_tol)
+  are_all_within_rtol <- all(abs_diff < rel_tol * pmax(abs(v), abs(w)))
+  return(are_all_within_atol && are_all_within_rtol)
+}
+
+logit_newton = function(design, outcome, ite = 1000){
+  n_pred = ncol(design)
+  beta_old = rep(0, n_pred)
+  for (i in 1:ite) {
+    hessian = logit_log_likelihood_hessian(beta_old, design, outcome)
+    gradient = logit_log_likelihood_gradient(beta_old, design, outcome)
+    beta_new = beta_old - solve(hessian, gradient)
+    loglik_old = logit_log_likelihood(beta_old, design, outcome)
+    loglik_new = logit_log_likelihood(beta_new, design, outcome)
+    if (are_all_close(loglik_old, loglik_new, n_pred / 100, n_pred / 100)) {
+      break
+    }
+    else{
+      beta_old = beta_new
+    }
+  }
+  return(beta_new)
+}
